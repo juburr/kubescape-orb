97fc835073f386744b270623d1229dc5057b5281
Justin Burr <juburr@users.noreply.github.com>
2024-08-26 03:15:16 +0000

    feat: add verbose argument to helm scan command

--- src/scripts/helm_scan_framework.sh
+++ src/scripts/helm_scan_framework.sh
@@ -10,6 +10,7 @@ NAMESPACE=$(circleci env subst "${PARAM_NAMESPACE}")
 OUTPUT_FILE=$(circleci env subst "${PARAM_OUTPUT_FILE}")
 OUTPUT_FORMAT="${PARAM_OUTPUT_FORMAT}"
 YAML_DIR="${HOME}/.local/share/kubescape-orb/yamls"
+VERBOSE=$(circleci env subst "${PARAM_VERBOSE}")
 
 # Print parameters for debugging purposes.
 echo "Running Kubescape framework scan..."
@@ -19,6 +20,12 @@ echo "  NAMESPACE: ${NAMESPACE}"
 echo "  OUTPUT_FILE: ${OUTPUT_FILE}"
 echo "  OUTPUT_FORMAT: ${OUTPUT_FORMAT}"
 echo "  YAML_DIR: ${YAML_DIR}"
+echo "  VERBOSE: ${VERBOSE}"
+
+VERBOSE_ARG=()
+if [[ "${VERBOSE}" == "1" ]]; then
+    VERBOSE_ARG=(--verbose)
+fi
 
 # Kubescape takes Kubernetes YAML files as input, but we just have a
 # Helm chart (.tar.gz file) at this point. Run the template command to
@@ -28,6 +35,7 @@ helm template "${CHART_PATH}" -n "${NAMESPACE}" > "${YAML_DIR}/chart.yaml"
 
 # Scan the YAML file using Kubescape
 kubescape scan framework "${FRAMEWORK}" \
+    "${VERBOSE_ARG[@]}" \
     --format "${OUTPUT_FORMAT}" \
     --output "${OUTPUT_FILE}" \
     "${YAML_DIR}/chart.yaml"
